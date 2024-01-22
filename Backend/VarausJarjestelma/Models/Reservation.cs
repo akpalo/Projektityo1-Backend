7b59ccb1d1a7f1be03972f3b88f693d1ee893858
akpalo <akseli.palo@student.lab.fi>
2024-01-22 10:16:54 +0200

    first

--- Backend/VarausJarjestelma/Models/Reservation.cs
+++ Backend/VarausJarjestelma/Models/Reservation.cs
@@ -3,8 +3,8 @@
     public class Reservation
     {
         public long Id { get; set; }
-        public User Owner { get; set; }
-        public Item Target { get; set; }
+        public virtual User? Owner { get; set; }
+        public virtual Item? Target { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
     }
