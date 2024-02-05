45af7c1d8162b79819dfa93259e778205055bb4c
akpalo <98313916+akpalo@users.noreply.github.com>
2024-02-05 20:18:22 +0200

    Delete projektityoRunko directory

--- projektityoRunko/varausjarjestelma/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { KalenteriComponent } from './kalenteri/kalenteri.component';
-
-export const routes: Routes = [
-    { path: 'kalenteri', component: KalenteriComponent }
-];
